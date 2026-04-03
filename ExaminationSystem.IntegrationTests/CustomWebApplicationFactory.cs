7c6999b6e1f4da3717091a1ab5ad3dbb03ccc619
Ahmed Fathy <ahmedfathy11876@gmail.com>
2026-04-03 17:08:26 +0200

    feat: add integration testing project and MediatR support

---
+++ ExaminationSystem.IntegrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,43 @@
+using System.Linq;
+using ExaminationSystem.Common.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ExaminationSystem.IntegrationTests;
+
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            // Remove the app's AppDbContext registration
+            var descriptor = services.SingleOrDefault(
+                d => d.ServiceType == typeof(DbContextOptions<AppDbContext>));
+
+            if (descriptor != null)
+            {
+                services.Remove(descriptor);
+            }
+
+            // Add AppDbContext using an in-memory database for testing
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseInMemoryDatabase("InMemoryDbForTesting");
+            });
+
+            // Build the service provider
+            var sp = services.BuildServiceProvider();
+
+            // Create a scope to obtain a reference to the database context
+            using var scope = sp.CreateScope();
+            var scopedServices = scope.ServiceProvider;
+            var db = scopedServices.GetRequiredService<AppDbContext>();
+
+            // Ensure the database is created
+            db.Database.EnsureCreated();
+        });
+    }
+}
