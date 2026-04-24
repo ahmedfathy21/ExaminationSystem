f3e71be6b0adf260d643089bce3320db0558923c
Ahmed Fathy <ahmedfathy11876@gmail.com>
2026-04-25 02:05:11 +0300

    feat: add MediatR dependency and implement GetQuiz endpoint in QuizController

--- Features/Quiz/QuizController.cs
+++ Features/Quiz/QuizController.cs
@@ -1,6 +1,14 @@
-namespace ExaminationSystem.Features.Quiz;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
 
-public class QuizController
+namespace ExaminationSystem.Features.Quiz;
+[ApiController]
+[Route("api/quiz")]
+public class QuizController : ControllerBase
 {
+    private readonly IMediator _mediator;
     
+    public QuizController(IMediator mediator) => _mediator = mediator;
+        
+    public async Task<IActionResult> GetQuiz(Guid id) => Ok(await _mediator.Send(new GetQuizQuery(id)));
 }
